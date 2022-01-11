10edecfa520af01f30a8e53037739cfc1f6bb003
BeterNerfIrelia <danieltudose150@yahoo.com>
2022-01-12 00:58:24 +0200

    [ADD] Visual header

--- game.py
+++ game.py
@@ -13,17 +13,21 @@ class Game:
     def __init__(self, board, screenSize, offset, timed):
         self.screen = None
         self.images = None
+        self.headerImages = {}
         self.board = board
         self.screenSize = screenSize
         self.offset = offset
         self.timed = timed
+        self.rects = []
         self.setPieceSize((self.screenSize[0], self.screenSize[1] - self.offset), self.board.getSize())
         setcwd()
         self.loadImages()
-
-    def run(self):
+        self.loadHeaderImages()
+        self.getRects()
         pygame.init()
         pygame.display.set_caption('Minesweeper')
+
+    def run(self):
         self.screen = pygame.display.set_mode(self.screenSize)
         running = True
         frameRate = pygame.time.Clock()
@@ -60,6 +64,8 @@ class Game:
                 self.screen.blit(image, topLeft)
                 topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
             topLeft = 0, topLeft[1] + self.pieceSize[1]
+
+        self.drawHeader()
 
     def loadImages(self):
         self.images = {}
@@ -106,3 +112,39 @@ class Game:
         if index[1] < 0 or index[1] >= self.board.getSize()[1]:
             return True
         return False
+
+    def loadHeaderImages(self):
+        self.headerImages["header"] = self.images["header"]
+        self.headerImages["header"] = pygame.transform.scale(self.headerImages["header"], (1000, 100))
+
+        self.headerImages["bg-block"] = self.images["bg-block"]
+        self.headerImages["bg-block"] = pygame.transform.scale(self.headerImages["bg-block"], (100, 50))
+
+        self.headerImages["exit"] = self.images["exit"]
+        self.headerImages["exit"] = pygame.transform.scale(self.headerImages["exit"], (50, 50))
+
+        self.headerImages["retry"] = self.images["retry"]
+        self.headerImages["retry"] = pygame.transform.scale(self.headerImages["retry"], (50, 50))
+
+    def drawHeader(self):
+        topLeft = (0, 0)
+        self.screen.blit(self.headerImages["header"], topLeft)
+
+        topLeft = (100, 25)
+        self.screen.blit(self.headerImages["bg-block"], topLeft)
+
+        topLeft = (800, 25)
+        self.screen.blit(self.headerImages["bg-block"], topLeft)
+
+        topLeft = (400, 25)
+        self.screen.blit(self.headerImages["retry"], topLeft)
+
+        topLeft = (550, 25)
+        self.screen.blit(self.headerImages["exit"], topLeft)
+
+    def getRects(self):
+        flags = pygame.Rect(100, 25, 100, 50)
+        self.rects.append(flags)
+
+        timer = pygame.Rect(800, 25, 100, 50)
+        self.rects.append(timer)
